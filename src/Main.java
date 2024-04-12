34041818d282bea176ba3564e8cc55c47f4dc548
irmaktekin <irmaktekin97@gmail.com>
2024-04-12 16:19:28 +0300

    Preworkout week

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        char char1 = 'J';
+        char char2 = 'A';
+        char char3 = 'V';
+        char char4 = 'A';
+
+        System.out.println(""+char1+char2+char3+char4);
+
+    }
+}
