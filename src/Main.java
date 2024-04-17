f44ca053ec23d70d3262ec380aa79937b5b7fd23
irmaktekin <irmaktekin97@gmail.com>
2024-04-17 20:56:39 +0300

    Constants

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        char char1 = 'J';
-        char char2 = 'A';
-        char char3 = 'V';
-        char char4 = 'A';
 
-        System.out.println(""+char1+char2+char3+char4);
+        Account account1 = new Account(12355);
+        Account account2 = new Account(23958);
+        System.out.println(account1.accountId);
+        System.out.println(account2.accountId);
+       /*this code will give a comp error, because we cannot assign a new value
+         to the final variable.
+         account1.accountId = 2234;*/
+
+        //both account1 and account2's rate are changed, because rate declares as static var.
+        account1.rate=30;
+        System.out.println(account1.rate);
+        System.out.println(account2.rate);
+        Account.rate= 50;
+        System.out.println(account1.rate);
+        System.out.println(account2.rate);
+
+        /*value will be assigned one memory location no matter how many instance of the class will be created
+        and can not be changed.
+        account1.odLimit = 3000;
+        Account.odLimit=3249; */
+
+
 
     }
+}
+class Account{
+    public Account(int id){
+        accountId=id;
+    }
+    final int accountId;
+    static double rate = 1.5;
+    static final int odLimit=1000;
+
 }
