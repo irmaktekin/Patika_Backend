e42b87fff465db6b3d31c9471a0ee3004bd5af8b
irmaktekin <irmaktekin97@gmail.com>
2024-04-24 01:24:59 +0300

    Area of the circle

--- src/Week1/CircleArea.java
+++ src/Week1/CircleArea.java
@@ -3,18 +3,17 @@ package Week1;
 import java.util.Scanner;
 
 public class CircleArea {
+    static final double PI = 3.14;
     public static void main(String[] args ){
-        final double PI = 3.14;
         Scanner input  = new Scanner(System.in);
         System.out.println("Enter the radius:");
         double radius = input.nextDouble();
 
         System.out.println("Enter the angle:");
         double angle = input.nextDouble();
+
         double area = (PI * (radius*radius)*angle)/360;
         System.out.println("Area of the circle is:"+area);
-
-
 
     }
 }
