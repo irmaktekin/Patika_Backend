3df1cf986dbad46cef62b43dd03f5aa90e82bdff
irmaktekin <irmaktekin97@gmail.com>
2024-04-24 01:04:25 +0300

    Area of the triangle

--- src/Week1/TriangleArea.java
+++ src/Week1/TriangleArea.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 
 public class TriangleArea {
     public static void main(String[] args){
+        double a,b,c,u,area;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the first edge:");
-        int a = input.nextInt();
+        a = input.nextDouble();
         System.out.println("Enter the second edge:");
-        int b= input.nextInt();
+        b= input.nextDouble();
         System.out.println("Enter the third edge:");
-        int c = input.nextInt();
+        c = input.nextDouble();
 
-        double u = (a+b+c)/2;
-        double area = Math.sqrt(u*(u-a)*(u-b)*(u-c));
-        System.out.println("Area of the triangle is: "+area);
+        // If one of the edge is 0 or negative number, don't calculate the area.
+        if(a<=0 || b<=0 || c<=0){
+            System.out.println("Edge values cannot be equal to 0 or smaller than 0.");
+        }
+        //If values are positive, calculate the area.
+        else{
+            //a+b+c -> 2u
+            u = (a+b+c)/2;
+            //area formula for the triangle when we have the edge values.
+            area = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+            System.out.println("Area of the triangle is: "+area);
+        }
     }
 }
