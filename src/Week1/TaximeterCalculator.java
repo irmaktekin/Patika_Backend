879967b3a2e8f7a5469e8a6d6988f3e087a1e991
irmaktekin <irmaktekin97@gmail.com>
2024-04-24 01:18:14 +0300

    Taximeter Calculator

--- src/Week1/TaximeterCalculator.java
+++ src/Week1/TaximeterCalculator.java
@@ -3,21 +3,24 @@ package Week1;
 import java.util.Scanner;
 
 public class TaximeterCalculator {
+    //because it cannot be changed during the program, they declared as a constant value.
+    static final int TAXIMETRE_BASE_PRICE=10;
+    static final double PER_KM_PRICE = 2.20;
     public static void main(String[] args){
-        final int TAXIMETRE_BASE_PRICE=10;
-        double perkmprice = 2.20;
-        double price;
+
         Scanner input = new Scanner(System.in);
         System.out.println("How many km does it take to the destination?");
         double km = input.nextDouble();
+        double totalprice = TAXIMETRE_BASE_PRICE+PER_KM_PRICE*km;
 
-
-        if(km<20){
-            price = 20;
+        //If total price is smaller than 20, the price will be 20.
+        if(totalprice<20){
+            totalprice = 20;
+            System.out.println(totalprice);
         }
+        //Otherwise, price per km will multiply with the km and added to the base price.
         else{
-            price = TAXIMETRE_BASE_PRICE+perkmprice*km;
+            System.out.println(totalprice);
         }
-        System.out.println(price);
     }
 }
