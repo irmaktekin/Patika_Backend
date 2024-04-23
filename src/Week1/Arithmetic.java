39ae1892ff806275167b0652b740d15d6583c9d5
irmaktekin <irmaktekin97@gmail.com>
2024-04-24 02:48:02 +0300

    Arithmetic Operations

--- src/Week1/Arithmetic.java
+++ src/Week1/Arithmetic.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class Arithmetic {
     public static void main(String [] args){
+        int num1,num2,num3,result;
         // Get three numbers from the user
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter three numbers: ");
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
+
+        System.out.print("Please enter a: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Please enter b: ");
+        num2 = scanner.nextInt();
+
+        System.out.print("Please enter c: ");
+        num3 = scanner.nextInt();
 
         // calculation for the inputs
-        int result = num1+num2*num3-num2;
+        result = num1+num2*num3-num2;
+
         //Display the result in the console
-        System.out.println(result);
+        System.out.println("Result is: "+result);
     }
 }
