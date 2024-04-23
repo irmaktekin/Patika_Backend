2442dba70ba46e5b61f075d09958904b4e7847fb
irmaktekin <irmaktekin97@gmail.com>
2024-04-23 21:43:35 +0300

    Calculate average grade

--- src/Week1/AverageGrade.java
+++ src/Week1/AverageGrade.java
@@ -5,38 +5,42 @@ import java.util.Scanner;
 public class AverageGrade {
 
     public static void main(String[] args ){
+        double mathGrade, physicGrade, chemistryGrade, turkishGrade, musicGrade,historyGrade;
+        double gradeSum, averageGrade;
 
         //Create scanner object
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your Math grade:");
 
         // Get the input from the user
-        int mathGrade = input.nextInt();
+        System.out.println("Please enter your Math grade:");
+        mathGrade = input.nextDouble();
+
         System.out.println("Please enter your Physic grade:");
+        physicGrade = input.nextDouble();
 
-        int physicGrade = input.nextInt();
         System.out.println("Please enter your Chemistry grade:");
+        chemistryGrade = input.nextDouble();
 
-        int chemistryGrade = input.nextInt();
         System.out.println("Please enter your Turkish grade:");
+        turkishGrade = input.nextDouble();
 
-        int turkishGrade = input.nextInt();
         System.out.println("Please enter your Music grade:");
+        musicGrade = input.nextDouble();
 
-        int musicGrade = input.nextInt();
         System.out.println("Please enter your History grade:");
+        historyGrade = input.nextDouble();
 
-        int historyGrade = input.nextInt();
+        //Assigning sum of the grades to a variable
+        gradeSum = mathGrade+physicGrade+chemistryGrade+turkishGrade+musicGrade+historyGrade;
 
         // Find the average grade
-        double averageGrade = (mathGrade+physicGrade+chemistryGrade+turkishGrade+musicGrade+historyGrade)/6;
+        averageGrade = gradeSum/6;
         System.out.println(averageGrade);
 
-        String result = (averageGrade>60) ?("Sınıfı Geçti.") : ("Sınıfta Kaldı.");
+        // If the grade is 60 or higher, it prints -> "Sınıfı Geçti."
+        // If the grade is smaller than 60, it prints -> "Sınıfta Kaldı."
+        String result = (averageGrade>=60) ?("Sınıfı Geçti.") : ("Sınıfta Kaldı.");
         System.out.println(result);
-
-
-
 
     }
 }
