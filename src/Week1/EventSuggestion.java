abdaf00663e859d630b3eb394d6d092242515695
irmaktekin <irmaktekin97@gmail.com>
2024-04-25 20:54:21 +0300

    Event Suggestion

---
+++ src/Week1/EventSuggestion.java
@@ -0,0 +1,31 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class EventSuggestion {
+    public static void main(String[] args) {
+
+        /*the temperature will be stored in this variable*/
+        int temp;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the weather temperature?");
+        temp=input.nextInt();
+
+        /*If temperature is greater than 5*/
+        if(temp<5){
+            System.out.println("Skating");
+        }
+        /*If temperature is between 5 and 15*/
+        else if(temp>=5 && temp<=15){
+            System.out.println("Cinema");
+        }
+        /*If temperature is between 15 and 25*/
+        else if(temp>15 && temp<=25){
+            System.out.println("Picnic");
+        }
+        /*If temperature is greater than 25*/
+        else{
+            System.out.println("Swimming");
+        }
+    }
+}
