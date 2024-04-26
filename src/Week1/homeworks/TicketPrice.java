96acbf6c51d012bb0654bb0d17ef735cd9fb636e
irmaktekin <irmaktekin97@gmail.com>
2024-04-26 11:51:41 +0300

    Ticket Price

--- src/Week1/homeworks/TicketPrice.java
+++ src/Week1/homeworks/TicketPrice.java
@@ -6,11 +6,10 @@ public class TicketPrice {
     /*Price per km declared as static and final because it is a constant value.*/
     public static final float PRICE_PER_KM = 0.10f;
     public static void main(String[] args) {
-        float km,roundtripdiscount,price=0;
+        float km,roundtripdiscount,price=0, ageDiscount=0;
         int age;
         int flightType;
 
-        /*Get input from the user*/
         Scanner input = new Scanner(System.in);
         System.out.println("How many km's will you take?");
         km = input.nextFloat();
@@ -22,7 +21,6 @@ public class TicketPrice {
         flightType = input.nextInt();
 
         price =(km * PRICE_PER_KM);
-        float ageDiscount;
 
         //If km or age is negative print->"Hatalı veri"
         if(km<=0||age<=0){
@@ -33,64 +31,39 @@ public class TicketPrice {
             System.out.println(flightType);
             System.out.println("Hatalı veri girdiniz");
         }
-        
-        //if flight type 2, first apply %20 discount.
+
+        //check the flight type
         else if (flightType == 2) {
+
             //different discounts according to ages
             if (age < 12) {
                 /*discount for the age*/
                 ageDiscount=price*0.5f;
-                price-=ageDiscount;
-
-                /*calculate roundtrip discount price*/
-                roundtripdiscount=(price*0.4f);
-
-                /*calcculate price with roundtrip discount
-                * multiply with 2 because it is a roundtrip(2 ticket)*/
-                price=(price-roundtripdiscount)*2;
-                System.out.println("Total price:"+price);
-
             } else if (age >= 12 && age <= 24) {
                 ageDiscount=price*0.10f;
-                price-=ageDiscount;
-                roundtripdiscount=(price*0.20f);
-                price=(price-roundtripdiscount)*2;
-                System.out.println("Total price:"+price);
             } else if (age > 65) {
                 ageDiscount=price*0.30f;
-                price-=ageDiscount;
-                roundtripdiscount=(price*0.4f);
-                price=(price-roundtripdiscount)*2;
-                System.out.println("Total price:"+price);
             }
-            else{
-                /*roundtrip discount will be applied, but not age discount will be applied.
-                 * age 25<=age<=65*/
-                roundtripdiscount=(price*0.4f);
-                price=(price-roundtripdiscount)*2;
-                System.out.println("Total price:"+price);
-            }
+            //Subtract age discount from total price
+            price = price-ageDiscount;
+            //Apply roundtrip discount and subtract from the total price, multiply with 2 for roundtrip
+            roundtripdiscount=(price)*0.20f;
+            price=(price-roundtripdiscount)*2;
+            System.out.println("Total price:"+price);
         }
+
         //if flight 1, discount will change according to age.
         else{
             if (age < 12) {
                 ageDiscount = price * 0.50f;
-                price-=ageDiscount;
-                System.out.println("Total price:"+price);
             } else if (age >= 12 && age <= 24) {
                 ageDiscount = price * (0.10f);
-                price-=ageDiscount;
-                System.out.println("Total price:"+price);
             } else if (age > 65) {
                 ageDiscount = price * (0.30f);
-                price-=ageDiscount;
-                System.out.println("Total price:"+price);
             }
-            /*It is not roundtrip and also no discount for that age range so the price remain same.
-             age 25<=age<=65*/
-            else{
-                System.out.println("Total price:"+price);
-            }
+            // Age discount will subtract from the price
+            price-=ageDiscount;
+            System.out.println("Total price:"+price);
         }
     }
 }
