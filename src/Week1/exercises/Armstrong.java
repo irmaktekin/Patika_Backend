696d392b7612b705166ad1a274f700b0d9e2bade
irmaktekin <irmaktekin97@gmail.com>
2024-04-28 01:17:18 +0300

    Armstrong

---
+++ src/Week1/exercises/Armstrong.java
@@ -0,0 +1,22 @@
+package Week1.exercises;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int sum=0;
+
+        while(number!=0){
+            //the last digit which remains from the divided by 10
+            int lastDigit = number%10;
+            //add digit to the sum
+            sum+=lastDigit;
+            //extract the last digit from the number
+            number/=10;
+        }
+        System.out.println("Sum is: "+ sum);
+    }
+}
+
