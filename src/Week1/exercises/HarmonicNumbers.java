12fbc636fa1c8209f8f99784afdd85ea6b78e34f
irmaktekin <irmaktekin97@gmail.com>
2024-04-28 01:27:46 +0300

    Harmonic Numbers

--- src/Week1/exercises/HarmonicNumbers.java
+++ src/Week1/exercises/HarmonicNumbers.java
@@ -1,2 +1,21 @@
-package Week1.exercises;public class HarmonicNumbers {
+package Week1.exercises;
+
+import java.util.Scanner;
+
+public class HarmonicNumbers
+{
+    public static void main(String[] args) {
+        //Get the input from the user
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number = input.nextInt();
+        float result=0;
+
+        //(1/1+1/2+.....1/i)
+        for(double i=1;i<=number;i++){
+            result += (1/i);
+        }
+        //Print the result
+        System.out.printf("Result is %.1f",result);
+    }
 }
