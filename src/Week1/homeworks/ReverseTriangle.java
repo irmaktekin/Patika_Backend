47b86c64f4896435cfca541b8d4567fd496adc75
irmaktekin <irmaktekin97@gmail.com>
2024-04-28 16:10:51 +0300

    Reverse Trianle

--- src/Week1/homeworks/ReverseTriangle.java
+++ src/Week1/homeworks/ReverseTriangle.java
@@ -8,23 +8,17 @@ public class ReverseTriangle {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of digits:");
         int number = input.nextInt();
-        int numSpaces=number-1;
-
         //While loop for the line numbers(For ex;If user enter 10, it will display 10 different line.)
-        numSpaces=0;
-        for(int i=number;i>0;i--){
-            for (int j = 0; j <= numSpaces; j++)
-                System.out.print(" ");
-            //print * number times(n->3, ***)
-            for(int j=1;j<number;j++) {
-                System.out.print("* ");
-            }
-            //increment space, because the other line will 1 additional space from the previous line
-            numSpaces++;
-            //It is for enter a new line.
-            System.out.println();
-            //decrement number so the other line will 1 * less.(**)
-            number-=1;
-        }
+            int i=0;
+                while(i<number){
+                    //For loop for print * in a line
+                    for(int j=1;j<number*2;j++) {
+                        System.out.print("*");
+                    }
+                    //It is for enter a new line, and continue to print with *
+                    System.out.println();
+                    //If the first line have 19 *, next line will be have 17 *.
+                    number-=1;
+                }
     }
 }
