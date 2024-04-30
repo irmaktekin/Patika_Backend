7fffcbef4c5019ee0b4143f2eb4b9fea013e5d3b
irmaktekin <irmaktekin97@gmail.com>
2024-05-01 00:00:04 +0300

    Prime Numbers

---
+++ src/Week2/exercises/PrimeNumber.java
@@ -0,0 +1,34 @@
+package Week2.exercises;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static boolean isPrime(int number, int division){
+        int result = number%division;
+        //if number 2-> it is always prime
+        if((number==2)||(division==number)){
+            return true;
+        }
+        //if the remainder 0 or number 1, it is not prime
+        else if(number==1 || result==0){
+            return false;
+        }
+        //continue to call itself
+        else{
+            return isPrime(number,division+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number = input.nextInt();
+        //method call
+        boolean result = (isPrime(number,2));
+        //print
+        System.out.println("The number is " + (result? "prime" : "not-prime"));
+
+    }
+}
+
