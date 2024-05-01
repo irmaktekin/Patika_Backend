d483fc5781e65e1c54eac480c94ca90b2c2496d1
irmaktekin <irmaktekin97@gmail.com>
2024-05-01 14:22:30 +0300

    Pattern

--- src/Week2/homeworks/Pattern.java
+++ src/Week2/homeworks/Pattern.java
@@ -9,7 +9,8 @@ public class Pattern {
     a->once the number is negative, it will set to 1.
     firstNumber -> holds the initial value for not exceed the number.
      */
-    public static void createPattern(int number,int a,int firstNumber){
+    static int firstNumber;
+    public static void createPattern(int number,int a){
         System.out.println(number);
 
         /* if the value is negative before and if the capacity will not exceed
@@ -17,26 +18,26 @@ public class Pattern {
         */
         if(a!=0){
             if(number+5<=firstNumber){
-                createPattern(number+5,a,firstNumber);
+                createPattern(number+5,a);
             }
         }
         //number is negative and capacity will not be exceeded
         else if(((number)<=0)) {
-            createPattern(number+5,1,firstNumber);
+            createPattern(number+5,1);
 
         }
         //number is equal or greater than 0 and never been negative before.
         else{
-            createPattern(number-5,0,firstNumber);
+            createPattern(number-5,0);
         }
     }
     public static void main(String[] args) {
         //get input
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number:");
-        int number = input.nextInt();
+        firstNumber = input.nextInt();
         //method call
-        createPattern(number,0,number);
+        createPattern(firstNumber,0);
     }
 }
 
