371a7e1c1877c6d31d8eb2f9f7d9b5c17eb8d166
irmaktekin <irmaktekin97@gmail.com>
2024-05-02 16:52:38 +0300

    Box Game

--- src/Week2/exercises/boxgame/Main.java
+++ src/Week2/exercises/boxgame/Main.java
@@ -1,2 +1,11 @@
-package Week2.exercises.boxgame;public class Main {
+package Week2.exercises.boxgame;
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.findFighter();
+
+    }
 }
