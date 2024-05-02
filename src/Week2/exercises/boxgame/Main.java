4741f1a04008121fa0e7c48e599e608ed8669263
irmaktekin <irmaktekin97@gmail.com>
2024-05-02 16:54:30 +0300

    Box Game

--- src/Week2/exercises/boxgame/Main.java
+++ src/Week2/exercises/boxgame/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
         Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
         Ring r = new Ring(marc,alex , 90 , 100);
+        //it finds the startes and call the run method
         r.findFighter();
 
     }
