29234dc1a22a6cddb16d53ac8ef1805e37e9747f
irmaktekin <irmaktekin97@gmail.com>
2024-05-06 23:27:15 +0300

    B Letter with *

--- src/Week2/exercises/BWithStars.java
+++ src/Week2/exercises/BWithStars.java
@@ -2,10 +2,11 @@ package Week2.exercises;
 
 public class BWithStars{
 
-
     public static void main(String[] args) {
+        //Matrix is declared with 7 rows and 4 columns.
         String[][] letter = new String[7][4];
 
+        //for the specific i values, * is assigned for creating B letter.
         for (int i = 0; i < letter.length; i++) {
             for (int j = 0; j < letter[i].length; j++) {
                 if (i == 0 || i == 3 || i == 6) {
@@ -18,6 +19,7 @@ public class BWithStars{
             }
         }
 
+        //It prints the pattern
         for (String[] row : letter){
             for (String col : row){
                 System.out.print(col);
