392db576e1d38086ae19d0f240bd204500aad036
irmaktekin <irmaktekin97@gmail.com>
2024-05-06 22:55:00 +0300

    Sort Numbers

---
+++ src/Week2/homeworks/numbersorting/Main.java
@@ -0,0 +1,11 @@
+package Week2.homeworks.numbersorting;
+
+import static Week2.homeworks.numbersorting.SortNumbers.*;
+
+public class Main {
+    public static void main(String[] args) {
+        getUserInput();
+        //close the scanner object
+        input.close();
+    }
+}
