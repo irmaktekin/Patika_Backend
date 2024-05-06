f01b79d780448fd2da5541b49b4a96f2a61fe383
irmaktekin <irmaktekin97@gmail.com>
2024-05-07 00:48:38 +0300

    Palindrome Words

--- src/Week2/exercises/PalindromeWords.java
+++ src/Week2/exercises/PalindromeWords.java
@@ -7,9 +7,11 @@ public class PalindromeWords {
         String reversedWord="";
         boolean isPalindrome=false;
 
+        //it starts from the last character and adds to the start
         for (int i = word.length() - 1; i >= 0; i--) {
             reversedWord +=word.charAt(i);
         }
+        // if it is equal to the original string->palindrome
         if(reversedWord.equals(word)){
             isPalindrome =true;
         }
@@ -22,8 +24,8 @@ public class PalindromeWords {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the word:");
         String word = input.nextLine();
+        //It prints according to the result of the method call
         System.out.println(isPalindrome(word) ? word + " is a palindrome." : word + " is not a palindrome.");
-
     }
 }
 
