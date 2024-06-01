3e5057de623c68b77993ecd3a8d2891cd6b60873
irmaktekin <irmaktekin97@gmail.com>
2024-06-02 02:01:41 +0300

    Employees data operations

--- src/Week6/javadatabase/employees/DBConn.java
+++ src/Week6/javadatabase/employees/DBConn.java
@@ -18,28 +18,47 @@ public class DBConn {
         try {
             connect = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
             Statement st = connect.createStatement();
-            //Insert data
-            String sql1 = "insert into employees (employee_name, position,salary) values ('Chris', 'Accountant',5000)";
-            String sql2 = "insert into employees (employee_name, position,salary) values ('Tom', 'Project Manager',4000)";
-            String sql3 = "insert into employees (employee_name, position,salary) values ('Nick', 'Manager',3000)";
-            String sql4 = "insert into employees (employee_name, position,salary) values ('Suzanne', 'Manager',7000)";
-            String sql5 = "insert into employees (employee_name, position,salary) values ('Anna', 'Executive',3000)";
+            insertData(st);
 
+            ResultSet data = st.executeQuery(sql);
+            printData(data);
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void insertData(Statement st) {
+        //Insert data
+        String sql1 = "insert into employees (employee_name, position,salary) values ('Chris', 'Accountant',5000)";
+        String sql2 = "insert into employees (employee_name, position,salary) values ('Tom', 'Project Manager',4000)";
+        String sql3 = "insert into employees (employee_name, position,salary) values ('Nick', 'Manager',3000)";
+        String sql4 = "insert into employees (employee_name, position,salary) values ('Suzanne', 'Manager',7000)";
+        String sql5 = "insert into employees (employee_name, position,salary) values ('Anna', 'Executive',3000)";
+        try{
             st.executeUpdate(sql1);
             st.executeUpdate(sql2);
             st.executeUpdate(sql3);
             st.executeUpdate(sql4);
             st.executeUpdate(sql5);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
 
-            ResultSet data = st.executeQuery(sql);
+    }
+
+    private static void printData(ResultSet data) {
+        try{
             while(data.next()){
                 System.out.println("Id: "+data.getInt("id"));
                 System.out.println("Name: "+data.getString("employee_name"));
                 System.out.println("Name: "+data.getString("position"));
                 System.out.println("Name: "+data.getInt("salary"));
             }
-
-        } catch (SQLException e) {
+        }
+        catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
